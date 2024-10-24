907df785393e47a622f27bd0ed22f70966b3eab3
evgeny-dmi3ev <evgeny.dmi3ev@gmail.com>
2024-10-24 13:11:22 +0200

    Revert "bump"
    This reverts commit 578f8b47ebe0e6ab4e55b23708312d60fb751c7e.

--- clearcache/urls.py
+++ clearcache/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
-from .views import ClearCacheAdminView, PurgeCFCache
+from .views import ClearCacheAdminView
 
 urlpatterns = [
-    url(r'clearcache/cf_cache/$', PurgeCFCache.as_view(), name="clearcache_cf_cache_admin"),
     url(r'clearcache/$', ClearCacheAdminView.as_view(), name="clearcache_admin"),
 ]
--- clearcache/views.py
+++ clearcache/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 
-from clearcache.forms import ClearCacheForm, PurgeCFCacheForm
+from clearcache.forms import ClearCacheForm
 from clearcache.utils import clear_cache
 
 
@@ -35,39 +35,3 @@ class ClearCacheAdminView(UserPassesTestMixin, FormView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Clear cache'
         return context
-
-
-class PurgeCFCache(ClearCacheAdminView):
-    form_class = PurgeCFCacheForm
-    template_name = "clearcache/admin/clearcache_form.html"
-
-    success_url = reverse_lazy('clearcache_cf_cache_admin')
-
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Purge CF Cache'
-        return context
-
-    def form_valid(self, form):
-        try:
-            urls = form.cleaned_data['urls']
-            purge_everything = form.cleaned_data['purge_everything']
-            from js_cloudflare.models import purge_cache, purge_all_cache, get_base_url
-            base_url = get_base_url()
-            urls = [base_url + url.strip() if url.startswith('/') else url.strip() for url in urls.split('\n')]
-            if purge_everything:
-                if purge_all_cache():
-                    messages.success(self.request, "Successfully purged all CF cache")
-                else:
-                    messages.error(self.request, "Couldn't purged CF cache, something went wrong")
-            else:
-                if purge_cache(urls):
-                    messages.success(self.request, "Successfully purged CF cache")
-                else:
-                    messages.error(self.request, "Couldn't purged CF cache, something went wrong")
-        except ImportError:
-            messages.error(self.request, "Couldn't purged CF cache, import error (js_cloudflare)")
-        except Exception as err:
-            messages.error(self.request, f"Couldn't purged CF cache, something went wrong. Received error: {err}")
-        return HttpResponseRedirect(self.success_url)
